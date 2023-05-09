8d49293e60ef8380dbfe6bbe181dd0cbc408bc61
hannah pelingon <hannahpelingon@hannahs-MacBook-Pro.local>
2023-05-08 21:42:58 -0400

    change made for test

--- src/routes/NotFound.js
+++ src/routes/NotFound.js
@@ -3,6 +3,7 @@ function NotFound(){
         <>
             <h1>Our Apologies!</h1>
             <p>This page is not found.</p>
+            <p>Please return to one of the main pages, thanks!</p>
         </>
     );
 }
